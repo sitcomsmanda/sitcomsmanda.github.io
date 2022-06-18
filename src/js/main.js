4fb0627d1e12a8e9cd8961b5bf2082ea696f83d4
DikDns <dikdnssocial@gmail.com>
2022-06-18 10:07:47 +0700

    🟢 add comments

--- src/js/main.js
+++ src/js/main.js
@@ -1,35 +1,38 @@
-const body = document.querySelector('body');
+/**
+ *!  Main Search Logics
+ */
 
-body.addEventListener('keyup', function(e) {
-    
-    // Define only for the input with search class
-    if (e.target.classList.contains('search')){
-        // Select and put it in a variables
-        const search = e.target.value.toUpperCase();
-        const li = document.querySelectorAll('.project');
+const body = document.querySelector("body");
 
-        // loop through the list
-        for (let i = 0; i < li.length; i++){
-            // select the text content
-            title = li[i].querySelector('.title');
-            sub = li[i].querySelector('.sub-title');
-            desc = li[i].querySelector('.desc');
-            // Insert all of them
-            txtValue = `${title.textContent || title.innerText} ${sub.textContent || sub.innerText} ${desc.textContent || desc.innerText}`
+body.addEventListener("keyup", function (e) {
+	// Define only for the input with search class
+	if (e.target.classList.contains("search")) {
+		// Select and put it in a variables
+		const search = e.target.value.toUpperCase();
+		const li = document.querySelectorAll(".project");
 
-            if (txtValue.toUpperCase().indexOf(search) > -1){
-                li[i].style.display = "";
-            } else {
-                li[i].style.display = 'none';
-            }
-        }
+		// loop through the list
+		for (let i = 0; i < li.length; i++) {
+			// select the text content
+			title = li[i].querySelector(".title");
+			sub = li[i].querySelector(".sub-title");
+			desc = li[i].querySelector(".desc");
+			// Insert all of them
+			txtValue = `${title.textContent || title.innerText} ${
+				sub.textContent || sub.innerText
+			} ${desc.textContent || desc.innerText}`;
 
-        // Remove the value if user press Escape
-        if(e.code === "Escape"){
-            e.target.value = '';
-            li.forEach(x => x.style.display = "");
-        }
+			if (txtValue.toUpperCase().indexOf(search) > -1) {
+				li[i].style.display = "";
+			} else {
+				li[i].style.display = "none";
+			}
+		}
 
-    }
-
+		// Remove the value if user press Escape
+		if (e.code === "Escape") {
+			e.target.value = "";
+			li.forEach((x) => (x.style.display = ""));
+		}
+	}
 });
