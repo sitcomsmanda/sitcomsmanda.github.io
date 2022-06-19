47880c11cbea64a4edc5f54663cfeb70da5b7ad3
DikDns <dikdnssocial@gmail.com>
2022-06-19 15:25:52 +0700

    add navSaintek and navSoshum logics

--- proyek/informasi-jurusan/src/js/main.js
+++ proyek/informasi-jurusan/src/js/main.js
@@ -1,20 +1,25 @@
 const main = async () => {
 	const main = document.querySelector("#main");
+	const navbarNav = document.querySelector(".navbar-nav");
 
 	// Path relative from index.html
 	const { saintek, soshum } = await getData("./data/informasi-jurusan.json");
 
 	let constructedRow = ``;
-
 	await saintek.forEach((jurusan) => {
 		constructedRow += constructRow(jurusan);
 	});
-
 	appendElement(constructedRow, main);
 
-	const nav = constructNavItem("Saintek", "fa-solid fa-flask", saintek);
+	const navSaintek = constructNavItem(
+		"Saintek",
+		"fa-solid fa-flask",
+		saintek
+	);
+	appendElement(navSaintek, navbarNav);
 
-	console.log(nav);
+	const navSoshum = constructNavItem("Soshum", "fa-solid fa-users", soshum);
+	appendElement(navSoshum, navbarNav);
 };
 
 try {
