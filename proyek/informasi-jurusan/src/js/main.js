07b3a3007376eb84820463ba41a7e3021b732a10
DikDns <dikdnssocial@gmail.com>
2022-07-04 11:42:00 +0700

    🟢 add: error handling

--- proyek/informasi-jurusan/src/js/main.js
+++ proyek/informasi-jurusan/src/js/main.js
@@ -14,28 +14,52 @@ const main = async () => {
 	const navbarNav = document.querySelector(".navbar-nav");
 
 	// Path relative from index.html
-	const { saintek, soshum } = await getData("./data/informasi-jurusan.json");
-
-	let constructedRow = ``;
-
-	await saintek.forEach((fakultas) => {
-		constructedRow += constructRow(fakultas);
-	});
-
-	await soshum.forEach((fakultas) => {
-		constructedRow += constructRow(fakultas);
-	});
-
-	const navSaintek = constructNavItem(
-		"Saintek",
-		"fa-solid fa-flask",
-		saintek
+	const info = await getData("./data/informasi-jurusan.json").catch(
+		(error) => {
+			console.error(error);
+		}
 	);
-	const navSoshum = constructNavItem("Soshum", "fa-solid fa-users", soshum);
 
-	appendElement(constructedRow, main);
-	appendElement(navSaintek, navbarNav);
-	appendElement(navSoshum, navbarNav);
+	if (info) {
+		const { saintek, soshum } = info;
+
+		let constructedRow = ``;
+
+		await saintek.forEach((fakultas) => {
+			constructedRow += constructRow(fakultas);
+		});
+
+		await soshum.forEach((fakultas) => {
+			constructedRow += constructRow(fakultas);
+		});
+
+		const navSaintek = constructNavItem(
+			"Saintek",
+			"fa-solid fa-flask",
+			saintek
+		);
+		const navSoshum = constructNavItem(
+			"Soshum",
+			"fa-solid fa-users",
+			soshum
+		);
+
+		appendElement(constructedRow, main);
+		appendElement(navSaintek, navbarNav);
+		appendElement(navSoshum, navbarNav);
+	} else {
+		const dangerAlert = `
+			<div class="row mt-5">
+				<div class="col mt-5">
+					<div class="alert alert-danger fs-3 fw-bold text-center" role="alert">
+						<i class="fa-solid fa-circle-exclamation me-2"></i>
+						Ada kesalahan ketika mengambil data!
+					</div>
+				</div>
+			</div>
+		`;
+		appendElement(dangerAlert, main);
+	}
 };
 
 try {
