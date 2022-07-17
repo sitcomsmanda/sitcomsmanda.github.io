4c63f67ee584c5765802ee6370980b194a449ee9
DikDns <dikdnssocial@gmail.com>
2022-07-17 08:41:37 +0700

    🟢 add: turu-bangun logic

--- proyek/turu-theme-toggle/src/js/main.js
+++ proyek/turu-theme-toggle/src/js/main.js
@@ -1,18 +1,19 @@
 const body = document.querySelector("body");
 const buttonCover = document.querySelector(".button-cover");
-const knobsSpan = document.querySelector(".button .knobs span");
+const header = document.querySelector(".header");
 let toggleKnobs = false;
 
 body.addEventListener("click", function (e) {
   if (e.target.classList.contains("checkbox")) {
     body.classList.toggle("dark");
     buttonCover.classList.toggle("dark");
-    // if (toggleKnobs) {
-    //   knobsSpan.innerHTML = `<i class="fa-solid fa-person-running"></i>`;
-    //   toggleKnobs = false;
-    // } else {
-    //   knobsSpan.innerHTML = `<i class="fa-solid fa-bed"></i>`;
-    //   toggleKnobs = true;
-    // }
+    header.classList.toggle("dark");
+    if (toggleKnobs) {
+      header.innerHTML = "Bangun";
+      toggleKnobs = false;
+    } else {
+      header.innerHTML = "Turu";
+      toggleKnobs = true;
+    }
   }
 });
